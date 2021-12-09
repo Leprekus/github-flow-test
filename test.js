52fe9751d494b63a81ae9083b81a5659e659a95e
leprekus <leprekus@yahoo.com>
2021-12-09 13:48:01 -0800

    created a test.js file and successfully tested git stash and git stash pop

---
+++ test.js
@@ -0,0 +1,11 @@
+console.log('hello world')
+
+/*
+ * i am writing some 
+ * dummy text. I want to stash this,
+ * change branch and then keep 
+ * editing it :)
+ * */
+
+//stashed this file, and then restored it with git stash drop after pulling 
+//some changes from the remote repo in main branch 
